533f2c474a71c7cc096de48f7cd0efef2096ceee
Harsh Barnawa <harshbarnawa.info@gmail.com>
2026-05-02 21:24:50 +0530

    finally solved cf617A

--- cf617A.cpp
+++ cf617A.cpp
@@ -4,8 +4,14 @@ using namespace std;
 int main() {
     int x = 13;
     int steps = 0;
+    int rmdr = x % 5;
 
-    cout << x/5;
+    if(rmdr == 0){
+        steps = x/5;
+    } else {
+        steps = x/5 + 1;
+    }
+    cout << steps;
 
     return 0;
 }
