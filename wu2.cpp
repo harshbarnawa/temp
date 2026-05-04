ccd1d22f1c4e6d1e41a4550eb512d9abb4a54a86
Harsh Barnawa <harshbarnawa.info@gmail.com>
2026-05-04 22:00:37 +0530

    Done the triple in just 2 minutes //easiest question ever

---
+++ wu2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int fac(int n){
+    if(n == 1){
+        
+        return 1;
+    }
+
+    return n * fac(n - 1);
+    
+}
+
+int main() {
+    int n = 5;
+
+    cout << fac(n);
+    return 0;
+}
